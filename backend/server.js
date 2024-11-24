2dfa50d0210356111307ebf34a2ab94b6fa7ed69
Sharafat Saqib <sharabalti62@gmail.com>
2024-11-24 22:56:15 +0500

    first commit

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 // Routes
 const postRoutes = require('./routes/posts');
-
 app.use('/api/posts', postRoutes);
 
-
-// Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Export Express app for Vercel
+module.exports = app;
